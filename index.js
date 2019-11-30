c00c8de9d3433b7338dab1dba112249a7349b4fb
Seothp <nikoejento@gmail.com>
2019-11-30 14:48:34 +0300

    add message with current move and winning functional

--- index.js
+++ index.js
@@ -1,56 +1,140 @@
 (function(){
-    const Player = (move) => {
-        return {move}
+    const Player = (name, move) => {
+        return {name, move}
     }
     
-    const Gameboard = {
-        board: [],
-        boardCellFabric () {
+    const Gameboard = () => {
+        let board = [];
+        const boardCellFabric = () => {
             let board = document.createElement('div');
             board.classList = 'board__cell';
             board.addEventListener('click', (e) => controller.doMove(e));
             return board;
-        },
-        generateBoard (num) {
+        }
+        const generateBoard = (num) => {
             for (let i = 0; i < num*num; i++) {
-                this.board.push(this.boardCellFabric());
+                board.push(boardCellFabric());
             }
-        },
-        renderBoard () {
-            let board = this.getBoardRoot();
-            for (let i = 0; i < this.board.length; i++) {
-                board.append(this.board[i]);
+        }
+        const renderBoard = () => {
+            gameboard.generateBoard(3);
+            let boardRoot = getBoardRoot();
+            for (let i = 0; i < board.length; i++) {
+                boardRoot.append(board[i]);
             }
-        },
-        getBoardRoot () {
+        }
+        const getBoardRoot = () => {
             return document.querySelector('.board');
-        },
-        
+        }
+        const getBoard = () => {
+            return board;
+        }
+        return {
+            boardCellFabric,
+            generateBoard,
+            renderBoard,
+            getBoardRoot,
+            getBoard,
+        }
     }
-    
-    const controller = {
-        players: [
-            Player("x"),
-            Player("o"),
-        ],
-        setFirstMove() {
-            this.currentPlayer = this.players[0];
-        },
-        switchPlayerMove() {
-            this.currentPlayer = this.currentPlayer == this.players[0] ? 
-                this.currentPlayer = this.players[1]: 
-                this.currentPlayer = this.players[0];
-        },
-        doMove (e) {
+
+    const Message = (() => {
+        const getMessageRoot = () => {
+            return document.querySelector('.message');
+        }
+        const getMessage = () => {
+            const name = Players.getCurrentPlayer().name;
+            return `${name}, твоя очередь ходить.`;
+        }
+        const renderMessage = (name) => {
+            const root = getMessageRoot();
+            root.innerHTML = getMessage();
+        }
+        return {
+            renderMessage,
+        }
+    })();
+    const Players = (() => {
+        let players = [];
+        const createPlayers = (names) => {
+            players = [Player(names[0], "x"), Player(names[1], "o")];
+        }
+        const getPlayers = () => {
+            return players;
+        }
+        const getCurrentPlayer = () => {
+            return Players.currentPlayer;
+        }
+        return {
+            getCurrentPlayer,
+            createPlayers,
+            getPlayers,
+        }
+    })();
+
+    const controller = (() => {
+        let players = [];
+        const startGame = () => {
+            gameboard.renderBoard();
+            players = Players.getPlayers();
+            Players.currentPlayer = players[0];
+        }
+        const switchPlayerMove = () => {
+            Players.currentPlayer = Players.currentPlayer == players[0] ? 
+            Players.currentPlayer = players[1]: 
+            Players.currentPlayer = players[0];
+            Message.renderMessage();
+            checkGameResult();
+            
+        }
+        const doMove = (e) => {
             let target = e.target;
             if(target.textContent === "") {
-                target.textContent = this.currentPlayer.move;
-                this.switchPlayerMove();
+                target.textContent = Players.currentPlayer.move;
+                switchPlayerMove();
             }
-        },
-    }
-    controller.setFirstMove();
-    Gameboard.generateBoard(3);
-    Gameboard.renderBoard();
-}());
+        }
+        const checkGameResult = () => {
+            const boardHTML = gameboard.getBoard().map((el)=>{
+                return el.innerHTML;
+            });
+            console.log(boardHTML);
+            if (boardHTML[0] !=='' && boardHTML[0] === boardHTML[1] && boardHTML[0] === boardHTML[2]) {
+                console.log("end game");
+            }
+            if (boardHTML[3] !=='' && boardHTML[3] === boardHTML[4] && boardHTML[3] === boardHTML[5]) {
+                console.log("end game");
+            }
+            if (boardHTML[6] !=='' && boardHTML[6] === boardHTML[7] && boardHTML[6] === boardHTML[8]) {
+                console.log("end game");
+            }
+            
+            if (boardHTML[0] !=='' && boardHTML[0] === boardHTML[3] && boardHTML[0] === boardHTML[6]) {
+                console.log("end game");
+            }
+            if (boardHTML[1] !=='' && boardHTML[1] === boardHTML[4] && boardHTML[1] === boardHTML[7]) {
+                console.log("end game");
+            }
+            if (boardHTML[2] !=='' && boardHTML[2] === boardHTML[5] && boardHTML[2] === boardHTML[8]) {
+                console.log("end game");
+            }
+            if (boardHTML[0] !=='' && boardHTML[0] === boardHTML[4] && boardHTML[0] === boardHTML[8]) {
+                console.log("end game");
+            }
+            if (boardHTML[2] !=='' && boardHTML[2] === boardHTML[4] && boardHTML[2] === boardHTML[6]) {
+                console.log("end game");
+            }
+        }
+        return {
+            checkGameResult,
+            startGame,
+            switchPlayerMove,
+            doMove,
+        }
+    })();
 
+    const gameboard = Gameboard();
+    Players.createPlayers(["Vanya", "Zaur"]);
+    controller.startGame();
+    Message.renderMessage();
+}());
