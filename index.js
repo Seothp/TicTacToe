0efa36a0bbf9c0bcf2b6c4278963ce6c0e3afbbf
Nikolay Geldyev <nikoejento@gmail.com>
2019-12-03 20:08:03 +0300

    add endGame and restart game functional

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 (function(){
+   
     const Player = (name, move) => {
         return {name, move}
     }
-    
+
+    const Operator = {
+        /*params = [
+            [firstX, firstY],
+            [secondX, secondY],
+            ...
+        ]
+        Need minimum 2 params.
+        */
+        isEqualMatrixVal(matrix, params) {
+            let result = 1;
+            for (let i = 0; i < params.length; i++) {
+                if (!(params.length <= i+1)){
+                    const first = matrix[params[i][0]][params[i][1]];
+                    const second = matrix[params[i + 1][0]][params[i + 1][1]];
+                    '' !== first && first === second ? result++: null;
+                }
+            }
+            return result === params.length;
+        }
+    }
+
     const Gameboard = () => {
         let board = [
             [],
@@ -24,6 +46,7 @@
             console.log(gameboard.getBoard());
         }
         const renderBoard = () => {
+            clearBoard();
             gameboard.generateBoard();
             let boardRoot = getBoardRoot();
             for (let i = 0; i < board.length; i++) {
@@ -53,6 +76,14 @@
             }
             return result;
         }
+        const clearBoard = () => {
+            getBoardRoot().innerHTML = "";
+            board = [
+                [],
+                [],
+                [],
+            ];
+        }
         return {
             getBoardInnerHtml,
             boardCellFabric,
@@ -75,8 +106,12 @@
             const root = getMessageRoot();
             root.innerHTML = getMessage();
         }
+        const clearMessage = () => {
+            getMessageRoot().innerHTML = "";
+        }
         return {
             renderMessage,
+            clearMessage,
         }
     })();
     const Players = (() => {
@@ -104,25 +139,51 @@
             players = Players.getPlayers();
             Players.currentPlayer = players[0];
         }
+        const endGame = () => {
+            gameboard.getBoardRoot().innerHTML = "Игра окончена";
+            Message.clearMessage();
+        }
         const switchPlayerMove = () => {
             Players.currentPlayer = Players.currentPlayer == players[0] ? 
             Players.currentPlayer = players[1]: 
             Players.currentPlayer = players[0];
             Message.renderMessage();
-            checkGameResult();
             
         }
-        const doMove = (e) => {
+        const doMove = async (e) => {
             let target = e.target;
             if(target.textContent === "") {
                 target.textContent = Players.currentPlayer.move;
+                checkGameResult();
                 switchPlayerMove();
             }
         }
         const checkGameResult = () => {
-            const boardInnerHtml = gameboard.getBoardInnerHtml()
-            isEqualMatrixVal(boardInnerHtml, [[0, 0], [0, 1], [0, 2]])
-            console.log(boardInnerHtml)
+            const boardInnerHtml = gameboard.getBoardInnerHtml();
+            const checkHorisontalLines = () => {
+                return (
+                    Operator.isEqualMatrixVal(boardInnerHtml, [[0, 0], [0, 1], [0, 2]]) ||
+                    Operator.isEqualMatrixVal(boardInnerHtml, [[1, 0], [1, 1], [1, 2]]) ||
+                    Operator.isEqualMatrixVal(boardInnerHtml, [[2, 0], [2, 1], [2, 2]])
+                )
+            }
+            const checkVericalLines = () => {
+                return (
+                    Operator.isEqualMatrixVal(boardInnerHtml, [[0, 0], [1, 0], [2, 0]]) ||
+                    Operator.isEqualMatrixVal(boardInnerHtml, [[0, 1], [1, 1], [2, 1]]) ||
+                    Operator.isEqualMatrixVal(boardInnerHtml, [[0, 2], [1, 2], [2, 2]])
+                )
+            }
+            const checkDiagonalLines = () => {
+                return (
+                    Operator.isEqualMatrixVal(boardInnerHtml, [[0, 0], [1, 1], [2, 2]]) ||
+                    Operator.isEqualMatrixVal(boardInnerHtml, [[0, 2], [1, 1], [2, 0]])
+                )
+            }
+            if (checkVericalLines() || checkHorisontalLines() || checkDiagonalLines()) {
+                console.log('game is end');
+                setTimeout(endGame, 1000);
+            }
         }
         return {
             checkGameResult,
@@ -131,6 +192,10 @@
             doMove,
         }
     })();
+
+    const btn = document.querySelector('.restart-game-btn');
+    btn.addEventListener('click',  controller.startGame);
+
 
     const gameboard = Gameboard();
     Players.createPlayers(["Vanya", "Zaur"]);
